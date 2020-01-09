026cece6698f8e8e450a4eb6f818ebd641a92a94
dora-lijun <hold_lijun@163.com>
2020-01-09 11:27:05 +0800

    update

--- leetcode-learning-array/src/main/java/cn/ipoo/App.java
+++ leetcode-learning-array/src/main/java/cn/ipoo/App.java
@@ -1,9 +1,8 @@
 package cn.ipoo;
 
-import org.junit.jupiter.api.Test;
-
 /**
- * Hello world!
+ * @author lijun
+ * @since 2020-01-07
  */
 public class App {
     public static void main(String[] args) {
@@ -23,7 +22,6 @@ public class App {
      * @param nums
      * @return
      */
-    @Test
     public static int removeRepeatArray(int[] nums) {
         if (nums == null || nums.length < 0) {
             return 0;
@@ -50,11 +48,14 @@ public class App {
      * 输出: 7
      * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
      * 随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6-3 = 3 。
+     * 此为贪心算法：在对问题求解时，总是做出在当前看来是最好的选择。也就是说，不从整体最优上加以考虑，他所做出的仅是在某种意义上的局部最优解。
+     * 贪心算法没有固定的算法框架，算法设计的关键是贪心策略的选择。必须注意的是，贪心算法不是对所有问题都能得到整体最优解，选择的贪心策略必须具备无后效性，即某个状态以后的过程不会影响以前的状态，只与当前状态有关。
+     * 所以对所采用的贪心策略一定要仔细分析其是否满足无后效性。
      *
      * @param prices
      * @return
      */
-    public int maxProfit(int[] prices) {
+    public static int maxProfit(int[] prices) {
         int profit = 0;
         for (int i = 1; i < prices.length; i++) {
             if (prices[i] - prices[i - 1] > 0) {
@@ -64,16 +65,22 @@ public class App {
         return profit;
     }
 
+    /**
+     * 旋转数组
+     * 题库算法-189题
+     * 初级算法-数组-03题
+     * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+     *
+     * @param nums
+     * @param k
+     */
     public static void rotate(int[] nums, int k) {
-        while (k > 0) {
-            System.out.println("k:" + k);
-            int temp = k;
-            nums[temp - k] = nums[nums.length - k];
-            k--;
-        }
+        int[] a = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            System.out.println(nums[i]);
-
+            a[(i + k) % nums.length] = nums[i];
+        }
+        for (int i = 0; i < a.length; i++) {
+            nums[i] = a[i];
         }
     }
 }
